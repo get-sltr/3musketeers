666a53174fa72eb821b880ef9e8094adf9a1fe30
get-sltr <kevsellsla@gmail.com>
2025-10-26 17:32:05 -0700

    Fix remaining @ alias import in app page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState, Suspense } from 'react'
-import { createClient } from '@/lib/supabase/client'
+import { createClient } from '../../lib/supabase/client'
 import { useRouter } from 'next/navigation'
 import FilterBar from '../../components/FilterBar'
 import MobileLayout from '../../components/MobileLayout'
